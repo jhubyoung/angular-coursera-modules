5f7a9974f96ba3e072a62748d6ff119c704daa61
jhubyoung <jhub1978@yahoo.com>
2016-10-23 12:38:29 -0700

    dont show image in my info page if no user info yet

--- module5-solution/src/public/signup/signup.service.js
+++ module5-solution/src/public/signup/signup.service.js
@@ -29,15 +29,12 @@ function SignupService($http){
   service.getDish = function(dishShortName){
     var menuUrl = "https://jhubyoung-course5.herokuapp.com/menu_items";
     menuUrl = menuUrl + "/"+dishShortName+".json";
-    console.log("service, getDish, URL: ", menuUrl);
     var promise = $http({
       url: menuUrl
     });
     promise.then(function(result){
-      console.log("service getDish, success: ", result);
       return result.data;
     }, function(errorResponse){
-      console.log("service getDish, error: ", errorResponse);
       return errorResponse;
     });
     return promise;
